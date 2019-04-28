4f3b8ab71f6a1f41ea62a1f5dacba24f6623d485
Jeff Wilkinson <j.wilkinson@queensu.ca>
2019-04-28 16:10:24 -0600

    Initial Commit

---
+++ Golf App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golf App
+//
+//  Created by Jeffrey Wilkinson on 2019-04-28.
+//  Copyright © 2019 Jeffrey Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
